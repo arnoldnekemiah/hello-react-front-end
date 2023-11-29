b12a08f93422aa2abd61337cfb1dbbedff8f768c
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-29 19:35:34 +0300

    configured linters

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,15 +4,10 @@ import API_BASE_URL from '../apiConfig';
 export const FETCH_RANDOM_GREETING = 'FETCH_RANDOM_GREETING';
 const API_ENDPOINT = `${API_BASE_URL}/greetings/random_greeting`;
 
-
 export const fetchRandomGreeting = () => async (dispatch) => {
-  try {
-    const response = await axios.get(API_ENDPOINT);
-    dispatch({
-      type: FETCH_RANDOM_GREETING,
-      payload: response.data.greeting,
-    });
-  } catch (error) {
-    console.error('Error fetching random greeting:', error);
-  }
+  const response = await axios.get(API_ENDPOINT);
+  dispatch({
+    type: FETCH_RANDOM_GREETING,
+    payload: response.data.greeting,
+  });
 };
